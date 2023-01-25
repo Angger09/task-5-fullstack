3015c0de47c3705741dd4ce4926bdee5c20c0739
Angger09 <anggerbagaswp@gmail.com>
2023-01-25 20:31:41 +0700

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
+public function run()
+{
+    factory(App\User::class, 50)->create();
+}
+
